c88994f297c65377d695b868defec76cb069acfa
maximka65 <maxim652002@gmail.com>
2022-12-08 11:57:22 +0300

    1

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Router />
     </BrowserRouter>
   </React.StrictMode>,
