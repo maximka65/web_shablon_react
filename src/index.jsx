b4c93176646264cfd11d8152c02e57b43bc31190
maximka65 <maxim652002@gmail.com>
2022-12-09 13:35:24 +0300

    try

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routes';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './routes'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Router />
     </BrowserRouter>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
